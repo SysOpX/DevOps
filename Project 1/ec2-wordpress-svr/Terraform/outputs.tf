624f90a5f6e7750f0641bfa5fcee99373a42bfa0
SysOpX <ericsonbrioso@gmail.com>
2025-10-12 14:17:03 +0800

    Change server name

--- Project 1/ec2-wordpress-svr/Terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_dns" {
-  description = "Public DNS of the web instance"
-  value       = aws_instance.web.public_dns
-}
-
-output "public_ip" {
-  description = "Public IP of the web instance"
-  value       = aws_instance.web.public_ip
-}
